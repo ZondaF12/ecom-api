aa708177cdab790910819be43720c2620d71ec72
zondaf12 <ruaridhbell73@googlemail.com>
2024-04-07 20:46:31 +0100

    Added Login route and JWT Auth

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.1
 require (
 	github.com/go-playground/validator/v10 v10.19.0
 	github.com/go-sql-driver/mysql v1.8.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.4
--- service/user/routes.go
+++ service/user/routes.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"github.com/zondaf12/ecom-api/config"
 	"github.com/zondaf12/ecom-api/service/auth"
 	"github.com/zondaf12/ecom-api/types"
 	"github.com/zondaf12/ecom-api/utils"
@@ -25,7 +26,34 @@ func (h *Handler) RegisterRoutes(router *echo.Group) {
 }
 
 func (h *Handler) HandleLogin(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Login")
+	// Parse payload
+	var payload types.LoginUserPayload
+	if err := utils.ParseJSON(c, &payload); err != nil {
+		return utils.WriteError(c, http.StatusBadRequest, err)
+	}
+
+	// Validate payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		return utils.WriteError(c, http.StatusBadRequest, fmt.Errorf("invalid payload %v", errors))
+	}
+
+	u, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		return utils.WriteError(c, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+	}
+
+	if !auth.ComparePassword(u.Password, []byte(payload.Password)) {
+		return utils.WriteError(c, http.StatusBadRequest, fmt.Errorf("not found, invalid email or password"))
+	}
+
+	secret := []byte(config.Envs.JWTSecret)
+	token, err := auth.CreateJWT([]byte(secret), u.ID)
+	if err != nil {
+		return utils.WriteError(c, http.StatusInternalServerError, err)
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"token": token})
 }
 
 func (h *Handler) HandleRegister(c echo.Context) error {
